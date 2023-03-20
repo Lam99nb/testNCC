51ea04cc9bfeaee4ff8db0c440a7943867053645
TNL <tathingoclam@gmail.com>
2023-03-21 03:51:11 +0700

    fix title

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log('lam');
+function myFunction() {
+    var x = document.getElementById("mySidebar");
+    if (x.className === "sidebar") {
+        x.className += " responsive";
+    } else {
+        x.className = "sidebar";
+    }
+}
